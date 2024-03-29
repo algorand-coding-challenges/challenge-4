ca76392f60a37e2c4a9273d2a718877f769d7a35
keerthilochankumar <keerthi.lochans@gmail.com>
2024-03-29 17:54:55 +0530

    solved challenge-4

--- challenge/index.ts
+++ challenge/index.ts
@@ -1,29 +1,14 @@
 import algosdk from "algosdk";
 import * as algokit from '@algorandfoundation/algokit-utils';
 
-// Set up algod client
 const algodClient = algokit.getAlgoClient()
 
-// Retrieve 2 accounts from localnet kmd
 const sender = await algokit.getLocalNetDispenserAccount(algodClient)
 
 const receiver = await algokit.mnemonicAccountFromEnvironment(
     {name: 'RECEIVER', fundWith: algokit.algos(100)},
     algodClient,
   )
-
-/*
-TODO: edit code below
-
-Description: 
-The below code is trying to atomically send 2 payment from sender to receiver 
-account by grouping them into an atomic transaction composer. 
-However, the code is not working. find and fix the bug so that the 2 payments are 
-successfully sent atomically.
-
-When solved correctly, the console should print out the following:
-"The first payment transaction sent 1000000 microAlgos and the second payment transaction sent 2000000 microAlgos"
-*/
 
 const suggestedParams = await algodClient.getTransactionParams().do();
 const ptxn1 = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
@@ -32,20 +17,15 @@ const ptxn1 = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
     to: receiver.addr,
     amount: 1000000,// 1 ALGO
 });
-
-/// <reference lib="dom" />
-
 const ptxn2 = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
     from: sender.addr,
     suggestedParams,
     to: receiver.addr,
-    amount: 2000000, // 2 ALGOs
+ amount: 2000000, // 2 ALGOs
 });
-
+const senderTransactionSigner = algokit.getSenderTransactionSigner(sender)
 const atc = new algosdk.AtomicTransactionComposer()
-atc.addTransaction({txn: ptxn1, signer: sender})
-atc.addTransaction({txn: ptxn2, signer: sender})
-
+atc.addTransaction({txn: ptxn1, signer: senderTransactionSigner})
+atc.addTransaction({txn: ptxn2, signer: senderTransactionSigner})
 const result = await algokit.sendAtomicTransactionComposer({atc:atc, sendParams: {suppressLog:true}}, algodClient)
 console.log(`The first payment transaction sent ${result.transactions[0].amount} microAlgos and the second payment transaction sent ${result.transactions[1].amount} microAlgos`)
-
